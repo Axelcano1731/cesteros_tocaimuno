b77028cce51c7c67a839690c8a50d72d6d3abdfa
Axelcano1731 <axelcano1711mail.com>
2024-04-07 01:30:23 -0500

    Se agrega el carrusel

---
+++ Js/Inicio.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 5 , 
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
