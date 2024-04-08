bb38813c82c9e08b36f659df6f1fac2f6d457554
Axelcano1731 <axelcano1711mail.com>
2024-04-08 01:10:09 -0500

    Se completa el inicio

--- Js/Inicio.js
+++ Js/Inicio.js
@@ -8,3 +8,18 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
+document.querySelector('.save_email .text').addEventListener('focus', function() {
+    this.style.opacity = '1';
+});
+
+document.querySelector('.save_email .text').addEventListener('blur', function() {
+    this.style.opacity = '.9';
+});
+
+var map = L.map('mapid').setView([4.457927227020264, -74.63422393798828], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+}).addTo(map);
+L.marker([4.457927227020264, -74.63422393798828]).addTo(map)
+    .bindPopup('Tocaima, Cundinamarca')
+    .openPopup();
